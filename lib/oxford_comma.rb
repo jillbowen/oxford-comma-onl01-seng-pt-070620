8a9b5bb90162c5d54ed2e6aef571498bbde124d4
Jill Short <jillian.b.short@gmail.com>
2020-07-15 05:09:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,18 @@
 def oxford_comma(array)
-  case array.length
-  when 1  
-    array.join
-  when 2 
-    array.join(" and ")
-  when => 3
-    array[-2] << " and "
-  end
+ if array.length = 1 
+   array.join
+ elsif array.length = 2 
+   array.join(" and ")
+  else array.length >= 3 
+    array[-1] << " and "
 end
+
+
+# case array.length
+#   when 1  
+#     array.join
+#   when 2 
+#     array.join(" and ")
+#   when => 3
+#     array[-2] << " and "
+#   end
